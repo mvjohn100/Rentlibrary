acf3e2b7e2b2b8f33ac63d8f2c20bd57a76c5e3d
John <john@klairvoyant.com>
2014-01-14 18:59:37 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rentlibrary::Application.config.secret_key_base = 'bdb7a1480b10c79b49338128593790e1a8555107ab03727cb3df3b7b25392492f0fb9ab43240977d67e388ca2db179b488cd8c408c89380e8e0f1c6bd13a1055'
